257b78f2c48caaf70801dedffa90072497b1efda
jjmebur@gmail.com <jjmebur@gmail.com>
2014-09-28 22:27:17 -0500

    fabrica alejandro ordoñez

---
+++ AbstractFactoryAutomovil/mundo/fabricas/FabricaToyota.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbstractFactoryAutomovil.mundo.interfaces;
+using AbstractFactoryAutomovil.mundo.clases;
+
+namespace AbstractFactoryAutomovil.mundo.fabricas
+{
+    class FabricaToyota : IFabricaDePartes
+    {
+        public IAuto crearAuto()
+        {
+            return new AutoToyota();
+        }
+
+        public IMotor crearMotor()
+        {
+            return new MotorToyota();
+        }
+
+        public ICarroceria crearCarroceria()
+        {
+            return new CarroceriaToyota();
+        }
+    }
+}
